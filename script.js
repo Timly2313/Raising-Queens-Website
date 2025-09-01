a95cb8e455a86fc61b54a0ce7b404186cc1f5bdd
Timly2313 <edmundshava2313@gmail.com>
2025-09-01 13:24:15 +0200

    Frame work added for everyone

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ menuToggle.addEventListener("click", () => {
     ? '<i class="fa-solid fa-xmark"></i>'
     : '<i class="fa-solid fa-bars"></i>';
 });
+
+
+    // progress bar
+  window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+
+  document.getElementById('progressBar').style.width = scrollPercent + '%';
+});
+
+    //contiunous sponsor scroll
+const track = document.querySelector('.carousel-track');
+let speed = 0.5;
+let position = 0;
+
+track.innerHTML += track.innerHTML;
+
+function animate() {
+  position -= speed;
+  if (position <= -track.scrollWidth / 2) {
+    position = 0;
+  }
+  track.style.transform = `translateX(${position}px)`;
+  requestAnimationFrame(animate);
+}
+
+animate()
